7889983a60176b19980b378d3812074c3ae05d64
sambathreasmey <reasmeysambath@gmail.com>
2025-08-24 10:54:59 +0700

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      const MyApp(
+
+      )
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
